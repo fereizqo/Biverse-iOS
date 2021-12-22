1ad6e322c33b6b253d4fc5f76dbd0bbe27c7d919
Fereizqo <fereizqoahmad23@gmail.com>
2021-12-22 16:29:25 +0700

    feat: initial commit

---
+++ Biverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Biverse
+//
+//  Created by Fereizqo Sulaiman on 22/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
